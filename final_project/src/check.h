5c8773573fbf2ca0210aca433ddf8b7daa452648
DavisLab <DavisLab@datalab.cs.nthu.edu.tw>
2020-12-29 19:25:58 +0800

    get input range

--- final_project/src/check.h
+++ final_project/src/check.h
@@ -1,13 +1,44 @@
-int quadrantX(int v, int q, int x, int y){
+int quadrant(int x, int y, int width, int high, int pace) {
+	int a = pace * width + pace;
+	int b = pace * high + pace;
 	if(x == 0){
-		return 1;
+		if(y > b / 2) {
+			return 2;
+		}else{
+			return 3;
+		}
 	}else if(y == 0){
-		return 2;
-	}else if(y == 0){
-		return 4;	
+		if(x > a / 2) {
+			return 4;
+		}else{
+			return 3;
+		}
+	}else if(x == a){
+		if(y > a / 2) {
+			return 1;
+		}else{
+			return 4;
+		}
+	}else if(y == b){
+		if(y > b / 2) {
+			return 1;
+		}else{
+			return 2;
+		}
 	}
 }
 
-bool cmp(int a, int b){
+bool cmp(int a, int b) {
 	return true;
 }
+
+double pace(int a, int b) {
+	if(a == b) {
+		return 1;
+	}else if(2 * a + 1 == b){
+		return 0.5;
+	}else{
+		return 0.25;
+	}
+}
+
--- final_project/src/main.cpp
+++ final_project/src/main.cpp
@@ -9,6 +9,7 @@ int main(){
 	string tmp;
 	CLB_Dim clb_dim;
 	Num_IO num_io;
+	int p;
 	int n;
 	map<string, int> input;
 	ifstream fin;
@@ -16,12 +17,47 @@ int main(){
 
 	fin >> tmp >> clb_dim.width >> clb_dim.high;
 	fin >> tmp >> num_io.width >> num_io.high;
-	fin >> n;
-	
+	fin >> tmp >> n;
+	p = pace(clb_dim.width, num_io.width);
+
+	int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
 	for(int i = 0; i < n; i++){
-		int x, y;
+		int x, y, q;
 		fin >> tmp >> x >> y;
+		q = quadrant(x, y, num_io.width, num_io.high, p);
+		
+		input[tmp] = q;
+		if(q == 1){
+			q1++;
+		}else if(q == 2){
+			q2++;
+		}else if(q == 3){
+			q3++;
+		}else{
+			q4++;
+		}
 	}
+	
+	fin >> tmp >> n;
+	for(int i = 0; i < n; i++){
+		int x, y, q;
+		fin >> tmp >> x >> y;
+		q = quadrant(x, y, num_io.width, num_io.high, p);
+		
+		input[tmp] = q;
+		if(q == 1){
+			q1++;
+		}else if(q == 2){
+			q2++;
+		}else if(q == 3){
+			q3++;
+		}else{
+			q4++;
+		}
+	}
+	
+	printf("%d:::%d:::%d:::%d>>%d", q1, q2, q3, q4, input.size());
+	
 	return 0;	
 }
 
