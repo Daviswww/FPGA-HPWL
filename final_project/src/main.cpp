be08a5bddc3059a199508a882ffd915c46bc8c25
DavisLab <DavisLab@datalab.cs.nthu.edu.tw>
2020-12-30 01:09:13 +0800

    map input

--- final_project/src/main.cpp
+++ final_project/src/main.cpp
@@ -6,36 +6,25 @@
 using namespace std;
 
 int main(){
-	string tmp;
+	string tmp, str, v[10000];
 	CLB_Dim clb_dim;
 	Num_IO num_io;
-	int p;
-	int n;
+	int p, n;
+	int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
 	map<string, int> input;
 	ifstream fin;
 	fin.open("../benchmarks/alu4_4.info");
 
 	fin >> tmp >> clb_dim.width >> clb_dim.high;
 	fin >> tmp >> num_io.width >> num_io.high;
-	fin >> tmp >> n;
 	p = pace(clb_dim.width, num_io.width);
-
-	int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
+	
+	fin >> tmp >> n;
 	for(int i = 0; i < n; i++){
 		int x, y, q;
 		fin >> tmp >> x >> y;
 		q = quadrant(x, y, num_io.width, num_io.high, p);
-		
 		input[tmp] = q;
-		if(q == 1){
-			q1++;
-		}else if(q == 2){
-			q2++;
-		}else if(q == 3){
-			q3++;
-		}else{
-			q4++;
-		}
 	}
 	
 	fin >> tmp >> n;
@@ -43,20 +32,32 @@ int main(){
 		int x, y, q;
 		fin >> tmp >> x >> y;
 		q = quadrant(x, y, num_io.width, num_io.high, p);
-		
 		input[tmp] = q;
-		if(q == 1){
-			q1++;
-		}else if(q == 2){
-			q2++;
-		}else if(q == 3){
-			q3++;
-		}else{
-			q4++;
+		cout << input[tmp] << endl;
+	}
+	fin.close();
+	fin.open("../benchmarks/alu4_4.nets");
+	fin >> n;
+	for(int i = 0; i < n; i++){
+		cout << ".";
+		int num = 0, set = 0;
+		getline(fin, str);
+		
+		while(str.find(" ") != -1){
+			v[num++] = tmp.assign(str, 0, str.find(" "));
+			str.erase(0, str.find(" ")+1);
+//			cout << v[num-1] << endl;
 		}
+		
+		cout << input[v[1]] << ":" << input[v[num]] << endl;
+//
+//		for(int i = 1; i < num; i++){
+//			input[v[i]] = set;
+//		}
 	}
 	
-	printf("%d:::%d:::%d:::%d>>%d", q1, q2, q3, q4, input.size());
+	cout << input.size();
+	printf("%d:::%d:::%d:::%d::::::%d", q1, q2, q3, q4, input.size());
 	
 	return 0;	
 }
