d9850df54bb1d4108c51e0ec0de55b3f478cda38
DavisLab <DavisLab@datalab.cs.nthu.edu.tw>
2020-12-31 12:53:56 +0800

    Num_PO小數點宣告

--- final_project/src/main.cpp
+++ final_project/src/main.cpp
@@ -4,43 +4,47 @@ using namespace std;
 
 int main(){
 	int count[5] = {0};
-	string tmp, str, v[10000];
+	string tmp, str, v[10000], inst[10000];
 	CLB_Dim clb_dim;
 	Num_IO num_io;
 	double p, x, y;
-	int q, n, m, num, set, Num_Inst;
+	int Num_PO, Num_PI;
+	int q, n, pi, po, num, set, Num_Inst = 0;
 	int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
 	map<string, int> input;
 	ifstream fin;
-	fin.open("../benchmarks/diffeq_4.info");
+	fin.open("../benchmarks/clma_4.info");
 	// CLB_Dim
 	fin >> tmp >> clb_dim.width >> clb_dim.high;
 	// Num_I/O_Pad
 	fin >> tmp >> num_io.width >> num_io.high;
 	p = pace(clb_dim.width, num_io.width);
 	// Num_PI
-	fin >> tmp >> n;
-	for(int i = 0; i < n; i++){
+	fin >> tmp >> Num_PI;
+	for(int i = 0; i < Num_PI; i++){
 		fin >> tmp >> x >> y;
 		q = quadrant(x, y, num_io.width, num_io.high, p);
 		input[tmp] = q;
 		count[q]++;
-		if(q == 0) {
-			cout << x <<":" << y << endl;
-		}
 	}
 	// Num_PO
-	fin >> tmp >> n;
-	for(int i = 0; i < n; i++){
-		int x, y, q;
+	fin >> tmp >> Num_PO;
+	for(int i = 0; i < Num_PO; i++){
 		fin >> tmp >> x >> y;
 		q = quadrant(x, y, num_io.width, num_io.high, p);
 		input[tmp] = q;
 		count[q]++;
+	}
+	// Num_Inst
+	fin >> tmp >> pi >> po;
+	Num_Inst = pi + po;
+	for(int i = 0; i < Num_Inst; i++){
+		fin >> tmp;
+		inst[i] = tmp;
 	}
 	fin.close();
 	// Nets
-	fin.open("../benchmarks/diffeq_4.nets");
+	fin.open("../benchmarks/clma_4.nets");
 	fin >> n;
 	for(int j = 0; j < n; j++){
 		num = 0, set = 0;
@@ -58,21 +62,24 @@ int main(){
 			}
 		}
 		for(int i = 1; i <= num; i++) {
+//			input[v[i]] = set;
 			if(input[v[i]] == 0) {
 				input[v[i]] = set;
 			}
 		}
 	}
-	cout << "GG" << endl;
-	for (map<string,int>::iterator it=input.begin(); it!=input.end(); ++it){
-		count[it->second]++;
+	cout << "done!" << endl;
+//	for (map<string,int>::iterator it=input.begin(); it!=input.end(); ++it){
+//		count[it->second]++;
+//	}
+	for(int i = 0; i < Num_Inst; i++){
+		count[input[inst[i]]]++;
 	}
 	// layout
 	for(int i = 0; i < 5; i++){
-		cout << count[i] << endl;
+		cout << i <<":::" <<count[i] << endl;
 	}
-	cout  <<  (clb_dim.width / 2) * (clb_dim.width / 2) * 2;
-	cout  <<  (clb_dim.high / 2) * (clb_dim.high / 2) * 2;
+	cout  << "block:" <<(clb_dim.width / 2) * (clb_dim.high / 2) * 2;
 	return 0;	
 }
 
